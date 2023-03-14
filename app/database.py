ccfb504ffb2614a036611cb97ca8bed5030bee1a
swetanshu <swetanshu84@gmail.com>
2023-03-14 19:31:31 +0530

    db change

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 # SQLALCHEMY_DATABASE_URL = 'postgresql://swetanshudubey:Sekhar123@localhost:5432/fastapi_db'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USER}:[{settings.DATABASE_PASSWORD}]:@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}:@{settings.DATABASE_HOST}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
